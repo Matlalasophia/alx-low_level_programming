dba96c0f86cac4f72a38f56b26100d669bf94347
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-16 11:27:19 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
-int main(void) {
+/* The main function*/
+int main(void)
+{
 int i;
 unsigned long ui;
 float f;
 double d;
 char c;
 long l;
-
-printf("Size of char: %zu bytes\n", sizeof(c));
-printf("Size of int: %zu bytes\n", sizeof(i));
-printf("Size of long int: %zu bytes\n", sizeof(ui));
-printf("Size of long long int: %zu byte\n", sizeof(d));
-printf("Size of float: %zu byte\n", sizeof(f));
-return 0;
+printf(
+		"Size of char: %zu bytes\n", sizeof(c));
+printf(
+		"Size of int: %zu bytes\n", sizeof(i));
+printf(
+		"Size of long int: %zu bytes\n", sizeof(ui));
+printf(
+		"Size of long long int: %zu byte\n", sizeof(d));
+printf(
+		"Size of float: %zu byte\n", sizeof(f));
+return (0);
 }
