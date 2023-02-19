1b14f54917993640ceaf9261762cc2fe2c2cba3a
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-19 05:58:12 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints alphabetic in lowercase
+ * Return:0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch == 'q' || ch == 'e')
+{
+ch++;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
