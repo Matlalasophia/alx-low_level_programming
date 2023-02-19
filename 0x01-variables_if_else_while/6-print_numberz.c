f909507fe7ab790d5e1ec6ff45e440c0b4b97aa8
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-19 06:16:58 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints alphabetic in lowercase
+ * Return:0
+ */
+int main(void)
+{
+int ch;
+for (ch = 0 ; ch <= 9; ch++)
+{
+putchar(ch);
+}
+putchar("\n");
+return (0);
+}
