4e1ca0b9676b3106057ef8128f8d4f70e7e3a541
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-20 09:35:04 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,17 @@ char a;
 char b;
 for (a = 48; a <= 57; a++)
 {
-      a=a;
-
+a = a;
 for (b = 48; b <= 57; b++)
-	{
-	putchar(a);
-	putchar(b);
-	if(a != 57 || b != 57){
-	putchar(44);
-	}
+{
+putchar(a);
+putchar(b);
+if (a != 57 || b != 57)
+{
+putchar(44);
 }
 }
-
+}
 putchar('\n');
 return (0);
 }
