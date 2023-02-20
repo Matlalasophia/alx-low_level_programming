c34e93a6aad89adbbb7b11f92af2ad777c807599
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-20 09:45:17 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,14 @@ int b;
 for (a = 48; a <= 57; a++)
 {
 a = a;
-for (b = 48; b <= 57; b++)
+for (b = 49; b <= 57; b++)
 {
 putchar(a);
 putchar(b);
+putchar(' ');
 if (a != 57 || b != 57)
 {
-putchar(44);
+putchar( 44);
 }
 }
 }
