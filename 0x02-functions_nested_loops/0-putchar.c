d0cfb1abc9e83b5749c513662b7f347abe7e3880
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-22 05:27:35 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include"main.h"
 /**
- * main - A program that print a line using the print function
- * Return: 0(Success)
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-printf("_putchar\n");
+-putchar('_');
+-putchar('p');
+-putchar('u');
+-putchar('t');
+-putchar('c');
+-putchar('h');
+-putchar('a');
+-putchar('r');
+-putchar('\n');
 return (0);
 }
+
