55efb548ac17ccd10665be5c73241c2ff18e7d56
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-24 06:50:15 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * positive_or_negative - print a number
+*Return:0
+*/
+void positive_or_negative(int i)
+{			
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+}
