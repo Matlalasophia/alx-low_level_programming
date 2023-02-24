58732ade86287621c81c20407fd98e36b90a2ea4
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-24 13:02:00 -0800

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/**
-* positive_or_negative - tests function that prints if int i is positive or negative
-*/
 void positive_or_negative(int i);
 #endif /* MAIN_H */
