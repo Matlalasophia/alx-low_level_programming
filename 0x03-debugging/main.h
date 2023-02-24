2d8227c40f4303a418648a6eb9ee70884f9d7045
Matlalasophia <sophiamoopelwa@gmail.com>
2023-02-24 12:54:59 -0800

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+/**
+* positive_or_negative - tests function that prints if int i is positive or negative
+* Return: i
+* Parameter: i 
+* i - must be greater than 0
+* Return - i
+*/
 int positive_or_negative(int i)
 {
 if (i > 0)
@@ -15,6 +22,6 @@ if (i < 0)
 {
 printf("%d is negative\n", i);
 }
-return (0);
+return (i);
 }
 #endif /* MAIN_H */
