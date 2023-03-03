62c9c06db965e8f53ca62b73224610413e3351f7
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-03 01:42:31 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - update the value of n to 98
+ * @n1 input
+ * Return: *n
+ */
+int reset_to_98(int *n)
+{
+int *n;
+*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,8 @@
+#include<unistd.h>
+/**
+ * _putchar - writes charecter c to stdout
+ * @c1 char to be printed
+ * Return: on success 1
+ */
+int _putchar(char c)
+return (write(1, &c, 1));
