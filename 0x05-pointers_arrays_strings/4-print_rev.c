509ed9edfbf06dea2875c5d90481de7634da7772
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-03 05:56:49 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,6 +9,7 @@ void print_rev(char *s)
 int longi = 0;
 int o;
 while (*s != '\0')
+{
 longi++;
 s++;
 }
@@ -16,7 +17,7 @@ s--;
 for (o = longi; o > 0; o--)
 {
 _putchar(*s);
-s++;
+s--;
 }
 _putchar('\n');
 }
