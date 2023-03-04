0ba6502af4d0e1bb75c07a5f8abfd4386a41ddf1
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-04 01:00:57 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
+int 1 = 0;
 int x = 0;
 
-while (*(src + i) != '\0')
+while (*(src + 1) != '\0')
 {
 i++
 }
-for (; x < i ; x++)
+for (; x < 1 ; x++)
 {
 dest[x] = src[x];
 }
-dest[i] = '\0';
+dest[1] = '\0';
 return (dest);
 }
