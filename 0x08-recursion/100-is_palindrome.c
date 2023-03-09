b2128374e81ac5f1bdf710202b5ea4d5c7cc3bd2
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-09 05:05:37 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is_palindrome - function that returns 1 if a string is a palindome
+ * @s: string to be tested
+ * Return: returns 1 if a string is palindrome
+ * otherwise returns 0 if number is not a palindrome
+ */
+int is_palindrome(char *s);
+{
+if (*s != '\0')
+{
+is_palindrome(*s);
+_putchar (*s);
+}
+}
+
