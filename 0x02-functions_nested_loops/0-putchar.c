e6206738cdb83b88cc44f322761f4f5b12389eda
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-10 08:09:04 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
-#include"main.h"
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-char strArr[] = {95, 112, 117, 116, 99, 104, 97, 114};
-int arrL = sizeof(strArr) / sizeof(strArr[1]);
-putcharMethod(strArr, arrL);
-fprintf(stderr, "[Anything]\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
