2ff269099d1d774f93c1935d40bfc132449054aa
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-12 06:11:08 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abc - function that computes the absolute value of an integer.
+ * @n: the int in queation
+ * Return: the abc of the int.
+ */
+int _abc(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
