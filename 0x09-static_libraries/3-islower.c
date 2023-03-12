53aaf7e673a7698b41bb6b7cb3e8aefc6d0e7b21
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-12 10:26:32 -0700

    9

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<unistd.h>
+/**
+ * _islower - check if a char is lowercase.
+ * Return: 1 if char is lowercase, otherwise return 0.
+ * @c: print return value
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
