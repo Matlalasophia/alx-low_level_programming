4c18cc916b4fabb930d62eccd54be5ba5e233664
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-20 12:02:40 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<stdbool.h>
 #include<ctype.h>
 #include<string.h>
 /**
  * main - program that adds positive numbers.
- * @argc: size of arguments
- * @argv: array arguments
  * Return: 1 if num contains symblos that are not digits, print Erro.
+ * @argc: Number of argumets
+ * @argv: Array of arguments
  */
 int main(int argc, const char *argv[])
 {
@@ -21,10 +20,6 @@ bool isSymbol = false;
 if (argc == 1)
 {
 printf("%s\n", "0");
-}
-if (argc == 2)
-{
-printf("%s\n", "Error");
 }
 if (argc >= 3)
 {
