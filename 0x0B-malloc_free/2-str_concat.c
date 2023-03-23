70df66028e4120508403f37ea57aaba6810b88f5
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-23 01:18:05 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,28 +8,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *conct;
-int i, ci;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-i = ci = 0;
-while (s2[ci] != '\0')
-i++;
-while (s2[ci] != '\0')
-ci++;
-conct = malloc(sizeof(char) * (i + ci + 1));
-if (conct == NULL)
-return (NULL);
-i = ci = 0;
-while (s1[i] != '\0')
+char *s3;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+while (s1 && s1[len1])
 {
-conct[i] = s1[i];
-i++, ci++;
+len1++;
 }
-conct[i] = '\0';
-return (conct);
+while (s2 && s2[len2])
+{
+len2++;
+s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 }
-
-
+if (s3 == NULL)
+{
+return (NULL);
+i =  0;
+j = 0;
+}
+while (i < len1)
+{
+while (i < (len1 + len2))
+{
+s3[i] = s2[j];
+i++;
+j++;
+}
+}
+s3[i] = '\0';
+return (s3);
+}
