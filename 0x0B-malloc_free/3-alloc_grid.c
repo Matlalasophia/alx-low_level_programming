2406ecb483c8760f4b98dddee9263bcf598107b2
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-24 04:44:38 -0700

    4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,39 +9,17 @@
 
 int **alloc_grid(int width, int height)
 {
-int **array;
-int i, j;
-if (height <= 0 || width <= 0)
+
+
+int gridArrayPointer[width][height]; 
+
+int gridSize = sizeof(gridArrayPointer[width][height]);
+
+int* sizeAllocated = &gridSize;
+if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-array = (int **) malloc(sizeof(int *) * height);
-if (array == NULL)
-{
-return (NULL);
+printf("%d\n", sizeAllocated);
+return (0) ;
 }
-for (i = 0; i < height; i++)
-{
-array[i] = (int *) malloc(sizeof(int) * width);
-}
-if (array[i] == NULL)
-{
-free(array);
-}
-for (j = 0; j <= i; j++)
-{
-free(array[j]);
-return (NULL);
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
-array[i][j] = 0;
-}
-}
-return (array);
-}
-
-
-
