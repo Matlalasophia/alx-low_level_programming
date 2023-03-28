26aa4d6e8a3db2f4b8088ff89a41f5ade12488f3
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-28 10:25:22 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,18 +22,27 @@ for (j = 0; j <= n; j++)
 sum  = i * j;
 if (j != n)
 {
-if ( sum <= 9){
-printf("%d,   ", sum);
-}else if(sum > 9 && sum <= 99)
+if ( sum <= 9)
 {
-printf("%d,  ", sum);
+printf("%d,   ",sum);
+}
+else if(sum > 9 && sum <= 99) 
+{
+printf("%d",sum);
+
+if (i * j+1 > 99){
+printf("%s",", ");
+}else{
+printf("%s",",  ");
+}
 } 
 else if(sum >= 99)
 {
-printf("%d, ", sum);
+printf("%d", sum);
+printf("%s",", ");
 }
 }
-if(j ==n )
+if(j == n )
 {
 printf("%d", sum);
 }
