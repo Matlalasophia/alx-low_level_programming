df656cf5a294915c73993a47661747f9b71e8124
Matlalasophia <sophiamoopelwa@gmail.com>
2023-03-29 09:26:00 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,7 +32,6 @@ half2 %= 10000000000;
 }
 printf("%lu%lu", half1, half2);
 if (count != 98)
-{
 printf(",");
 fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
@@ -41,5 +40,4 @@ fib2_half2 = half2;
 }
 printf("\n");
 return (0);
-}
 }
